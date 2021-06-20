b3e7134f7e5c898b2208938dd830e1492d45c152
Bimal Gharti Magar <bimal.gharti.magar@gmail.com>
2021-06-20 21:28:45 +0545

    Added tailwindcss as dev dependency
    * Updated nuxt config to use tailwindcss during build
    * Added lato font reference
    * Updated tailwind config to use lato font
    * Updated home page text

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,13 @@
 <template>
-  <div>Portfolio</div>
+<div class="home font-lato h-screen grid justify-center items-center bg-green-500">
+  <div class="title-wrapper">
+    <div class="p-4 text-6xl font-semibold">Hi, I'm Bimal</div>
+    <div class="p-4 text-4xl">I design and build web applications.</div>
+    <div class="p-4 text-3xl">I'm a software engineer at <a href="https://www.endpoint.com" target="_blank" referrerpolicy="no-referrer">Endpoint</a></div>
+    </div>
+    </div>
 </template>
+
+<style scoped>
+  @import url('https://fonts.googleapis.com/css?family=Lato:400,700&display=swap');
+</style>
