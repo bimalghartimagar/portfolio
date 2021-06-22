8a010f26a60c47c96cc5b67e00adc9a75e6e0f3c
Bimal Gharti Magar <bimal.gharti.magar@gmail.com>
2021-06-23 05:30:06 +0545

    styling changes to respect mobile first

--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,16 @@
 <template>
   <div
-    class="home font-lato h-screen grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 justify-items-center items-center bg-green-500"
+    class="home max-h-full grid grid-cols-1 md:grid-cols-2 justify-items-center items-center bg-green-500 font-lato h-screen "
   >
     <div class="title-wrapper p-4 text-center">
       <img
-        class="w-64 h-64 py-2 mx-auto"
+        class="w-96 h-96 py-2 mx-auto"
         src="~/assets/images/profile-outline.png"
         alt="Profile"
       />
-      <div class="py-2 xl:text-5xl lg:text-4xl md:text-4xl sm:text-8xl font-semibold">Hi, I'm Bimal.</div>
-      <div class="py-2 xl:text-4xl lg:text-2xl md:text-2xl sm:text-5xl">I design and build web applications.</div>
-      <div class="py-2 xl:text-3xl lg:text-2xl md:text-2xl sm:text-5xl">
+      <div class="py-2 text-7xl font-semibold">Hi, I'm Bimal.</div>
+      <div class="py-2 text-4xl">I design and build web applications.</div>
+      <div class="py-2 text-4xl">
         I work as a software engineer at
         <a
           class="cursor-pointer text-alabaster"
@@ -32,7 +32,7 @@
       </div>
     </div>
     <div>
-      <img class="stroke-current w-4/5 h-4/5 mx-auto" src="~/assets/images/work-illustration.svg" />
+      <img class="stroke-current w-3/5 h-3/5 mx-auto" src="~/assets/images/work-illustration.svg" />
     </div>
   </div>
 </template>
