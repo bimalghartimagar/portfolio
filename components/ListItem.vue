6ebe92fb28b9e710fc7535f9138e8af6a7b57581
Bimal Gharti Magar <bimal.gharti.magar@gmail.com>
2021-06-26 23:39:07 +0545

    Added projects page
    * Getting repos from Github API
    * filtering, mapping and sorting repos
    * mobile first design for repos

---
+++ components/ListItem.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <div>
+      <div class="font-semibold text-5xl mb-4">{{ repo.name }}</div>
+      <div class="text-3xl mb-4">{{ repo.description }}</div>
+    </div>
+    <div class="text-2xl">
+      {{ getDate(repo.created_at) }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    repo: {
+      type: Object,
+      required: false,
+    },
+  },
+
+  methods:{
+    getDate(date){
+      return date.split('T')[0]
+    }
+  }
+};
+</script>
---
+++ pages/projects.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="grid mt-10 justify-center items-center">
+    <aside class="w-60 text-center justify-self-end hidden">
+      <div class="rounded-full py-1.5 px-3 my-2 bg-alabaster">All</div>
+      <div class="rounded-full py-1.5 px-3 my-2 bg-alabaster">Backend</div>
+      <div class="rounded-full py-1.5 px-3 my-2 bg-alabaster">Frontend</div>
+    </aside>
+    <h3 class="text-5xl text-center mb-4 font-semibold">Github Repos, latest first.</h3>
+    <section class="grid grid-flow-row max-w-3xl">
+        <ListItem
+          class="p-4 my-2 rounded-md bg-alabaster h-56 flex flex-col justify-between"
+          v-for="repo in repos"
+          :key="repo.url"
+          :repo="repo"
+        />
+    </section>
+  </div>
+</template>
+
+<script>
+import ListItem from "../components/ListItem.vue";
+export default {
+  components: { ListItem },
+  data() {
+    return {
+      repos: [],
+    };
+  },
+  async fetch() {
+    this.repos = await fetch(
+      "https://api.github.com/users/bimalghartimagar/repos"
+    )
+      .then((res) => res.json())
+      .then(
+        (data) =>
+          (this.repos = data
+            .filter((item) => item.fork === false)
+            .map(({ created_at, url, name, description, tags_url }) => ({
+              created_at,
+              url,
+              name,
+              description,
+              tags_url,
+            }))
+            .sort((a,b)=>{
+              if (a.created_at>b.created_at) return -1
+              else if (a.created_at<b.created_at) return 1
+              else return 0
+            }))
+      );
+  },
+};
+</script>
