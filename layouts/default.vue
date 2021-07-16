7568637a3cc6842cc67f393a06eb8e972e2c4767
Bimal Gharti Magar <bimal.gharti.magar@gmail.com>
2021-07-16 20:20:21 +0545

    accessibility: updated bg color

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="bg-green-500 min-h-screen lg:grid lg:justify-items-stretch lg:items-baseline pt-4">
+  <div class="bg-green-600 min-h-screen lg:grid lg:justify-items-stretch lg:items-baseline pt-4">
     <TheNavBar />
     <main>
       <Nuxt />
